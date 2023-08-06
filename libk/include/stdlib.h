5224052acfc1e2e3b71913f84c0dc20e81b3d9f4
meisme-dev <omar.elantil@gmail.com>
2023-08-06 18:27:48 +0300

    Pragma once

--- libk/include/stdlib.h
+++ libk/include/stdlib.h
@@ -1,6 +1,5 @@
-#ifndef STDLIB_H
-#define STDLIB_H
+#pragma once
+
 #include <stddef.h>
 #include <stdint.h>
 void itoa(int64_t x, char *str);
-#endif
--- libk/include/sys/io.h
+++ libk/include/sys/io.h
@@ -1,5 +1,5 @@
-#ifndef IO_H
-#define IO_H
+#pragma once
+
 #include <stdint.h>
 uint32_t inl(uint16_t port);
 uint16_t inw(uint16_t port);
@@ -7,4 +7,3 @@ uint8_t inb(uint16_t port);
 void outl(uint16_t port, uint32_t value);
 void outw(uint16_t port, uint16_t value);
 void outb(uint16_t port, uint8_t value);
-#endif
