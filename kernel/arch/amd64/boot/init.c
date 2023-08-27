d1a7d4c679da1d89ea246779c40d31bfbe00ecaf
meisme-dev <omar.elantil@gmail.com>
2023-08-27 23:19:48 +0300

    Format

--- kernel/arch/amd64/boot/init.c
+++ kernel/arch/amd64/boot/init.c
@@ -2,10 +2,10 @@
 #include <arch/amd64/exception/idt.h>
 #include <arch/amd64/io/serial/serial.h>
 #include <common/exception/panic.h>
+#include <kernel.h>
 #include <memory/pmm.h>
 #include <terminal/log.h>
 #include <terminal/terminal.h>
-#include <kernel.h>
 
 void kinit() {
   if (!terminal_init() && !serial_init(COM1)) {
