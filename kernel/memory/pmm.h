c959b1d7f49177345016d5a9e44ebeac1e9c1da4
meisme-dev <omar.elantil@gmail.com>
2023-12-09 11:15:42 +0000

    Updated pmm (and comments)

--- kernel/memory/pmm.h
+++ kernel/memory/pmm.h
@@ -6,17 +6,33 @@
 
 #define PAGE_SIZE 4096
 
-enum page_types {
-  FREE,
-  USED
-};
+// enum page_types {
+//   FREE,
+//   USED
+// };
 
-typedef struct page_header_t {
-  enum page_types page_type;
+typedef struct page_descriptor_t {
+  // enum page_types page_type;
   uint64_t base;
-  struct page_header_t *next;
-} page_header_t __attribute__((aligned(PAGE_SIZE)));
+  struct page_descriptor_t *next;
+  struct page_descriptor_t *prev;
+} page_descriptor_t __attribute__((aligned(PAGE_SIZE)));
 
-page_header_t *pmm_init();
-page_header_t *pmm_alloc_page();
-void pmm_free_page(page_header_t *header);
+/***************************************************************************
+ * Initializes the physical memory manager
+ * @return The free list head
+ ***************************************************************************/
+page_descriptor_t *pmm_init(void);
+
+/***************************************************************************
+ * Allocates a page by removing its descriptor from the free list
+ * @return The page descriptor of the allocated page, may be NULL if no
+ *         suitable descriptor is found
+ ***************************************************************************/
+page_descriptor_t *pmm_alloc_page(void);
+
+/***************************************************************************
+ * Frees a page by inserting it in the free list
+ * @param[in] descriptor The descriptor to free
+ ***************************************************************************/
+void pmm_free_page(page_descriptor_t *descriptor);
