c90f174f0fa6fc8ab0250073e48963127f94d9b8
meisme-dev <omar.elantil@gmail.com>
2023-12-11 12:20:59 +0200

    Remove VMM test code

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -16,17 +16,6 @@ void kmain(void) {
     panic("FAILED TO INTIALIZE PHYSICAL MEMORY MANAGER");
   }
   cpu_init();
-  page_descriptor_t *page = vmm_alloc_mem(16 * PAGE_SIZE);
-  if (!page) {
-    panic("COULD NOT ALLOCATE MEMORY");
-  }
-  page_descriptor_t *current = page;
-  log(DEBUG, "Allocated pages:");
-  while (current->next) {
-    log(DEBUG, "    base: 0x%x, size: 0x%x", current->base, PAGE_SIZE);
-    current = current->next;
-  }
-  vmm_free_mem(page);
   asm volatile("int $0x80");
   cpu_halt();
 }
